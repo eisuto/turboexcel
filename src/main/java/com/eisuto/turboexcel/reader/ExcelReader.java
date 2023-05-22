67bc15fd047b7f4c9006576efe748c5c95583aa2
eisuto <waabdxiaoyi@gmail.com>
2023-05-22 18:13:25 +0800

    doc: 注释

--- src/main/java/com/eisuto/turboexcel/reader/ExcelReader.java
+++ src/main/java/com/eisuto/turboexcel/reader/ExcelReader.java
@@ -8,10 +8,19 @@ import java.io.File;
 import java.util.List;
 
 /**
+ * Excel 读取器
  * @author eisuto
  */
 public class ExcelReader {
 
+    private ExcelReader(){}
+
+    /**
+     * 读取 Excel 数据并将其转换为对象列表
+     * @param file Excel 文件
+     * @param clazz 映射类型
+     * @return 对象列表
+     */
     public static <T> List<T> read(File file, Class<T> clazz) {
         ExcelSheetOption sheetOption = ExcelSheetOption.valueOfClass(clazz);
         BaseExcelRowHandler<T> rowHandler = BaseExcelRowHandler.valueOfOption(sheetOption, clazz);
